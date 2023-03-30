8c933c93b1c016848827fbbfbe3083a613d10614
chris <chris@DESKTOP-U8R4UCN>
2023-03-30 17:18:27 -0500

    Add project files.

---
+++ ArtPicker/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace ArtPicker
+{
+    partial class artFinder
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(artFinder));
+            this.wordBar = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wordList = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerDisplay = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerInput = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.testButton = new System.Windows.Forms.Button();
+            this.wordDisplay = new System.Windows.Forms.Label();
+            this.timerInputSeconds = new System.Windows.Forms.NumericUpDown();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.skipButton = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.timerInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerInputSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wordBar
+            // 
+            this.wordBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wordBar.Location = new System.Drawing.Point(12, 26);
+            this.wordBar.MinimumSize = new System.Drawing.Size(162, 23);
+            this.wordBar.Name = "wordBar";
+            this.wordBar.PlaceholderText = "Dancer";
+            this.wordBar.Size = new System.Drawing.Size(162, 23);
+            this.wordBar.TabIndex = 1;
+            this.wordBar.Tag = "";
+            this.wordBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.wordBar_KeyDown);
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Location = new System.Drawing.Point(180, 26);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Word to Google";
+            // 
+            // wordList
+            // 
+            this.wordList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.wordList.FormattingEnabled = true;
+            this.wordList.ItemHeight = 15;
+            this.wordList.Location = new System.Drawing.Point(12, 55);
+            this.wordList.Name = "wordList";
+            this.wordList.Size = new System.Drawing.Size(120, 424);
+            this.wordList.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerDisplay
+            // 
+            this.timerDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerDisplay.AutoSize = true;
+            this.timerDisplay.Location = new System.Drawing.Point(649, 29);
+            this.timerDisplay.Name = "timerDisplay";
+            this.timerDisplay.Size = new System.Drawing.Size(73, 15);
+            this.timerDisplay.TabIndex = 4;
+            this.timerDisplay.Text = "timerDisplay";
+            this.timerDisplay.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Minutes and Seconds";
+            // 
+            // timerInput
+            // 
+            this.timerInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerInput.Location = new System.Drawing.Point(283, 26);
+            this.timerInput.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.timerInput.Name = "timerInput";
+            this.timerInput.Size = new System.Drawing.Size(81, 23);
+            this.timerInput.TabIndex = 7;
+            this.timerInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.timerInput.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Location = new System.Drawing.Point(479, 26);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(158, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(614, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // testButton
+            // 
+            this.testButton.Enabled = false;
+            this.testButton.Location = new System.Drawing.Point(479, -3);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 10;
+            this.testButton.Text = "Testing";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Visible = false;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // wordDisplay
+            // 
+            this.wordDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.wordDisplay.AutoSize = true;
+            this.wordDisplay.Location = new System.Drawing.Point(560, 5);
+            this.wordDisplay.Name = "wordDisplay";
+            this.wordDisplay.Size = new System.Drawing.Size(77, 15);
+            this.wordDisplay.TabIndex = 11;
+            this.wordDisplay.Text = "Word Display";
+            this.wordDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.wordDisplay.Visible = false;
+            // 
+            // timerInputSeconds
+            // 
+            this.timerInputSeconds.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerInputSeconds.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.timerInputSeconds.Location = new System.Drawing.Point(370, 26);
+            this.timerInputSeconds.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.timerInputSeconds.Name = "timerInputSeconds";
+            this.timerInputSeconds.Size = new System.Drawing.Size(67, 23);
+            this.timerInputSeconds.TabIndex = 12;
+            this.timerInputSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.deleteButton.Location = new System.Drawing.Point(35, 485);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 13;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // skipButton
+            // 
+            this.skipButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.skipButton.Location = new System.Drawing.Point(35, 485);
+            this.skipButton.Name = "skipButton";
+            this.skipButton.Size = new System.Drawing.Size(75, 23);
+            this.skipButton.TabIndex = 14;
+            this.skipButton.Text = "Skip";
+            this.skipButton.UseVisualStyleBackColor = true;
+            this.skipButton.Visible = false;
+            this.skipButton.Click += new System.EventHandler(this.skipButton_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(39, 517);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(69, 15);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Picture Raw";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // artFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.skipButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.timerInputSeconds);
+            this.Controls.Add(this.wordDisplay);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.timerInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timerDisplay);
+            this.Controls.Add(this.wordList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wordBar);
+            this.Controls.Add(this.addButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "artFinder";
+            this.Text = "Art Displayer";
+            this.Resize += new System.EventHandler(this.artFinder_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.timerInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerInputSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox wordBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox wordList;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerDisplay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown timerInput;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Label wordDisplay;
+        private System.Windows.Forms.NumericUpDown timerInputSeconds;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button skipButton;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ ArtPicker/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unsplasharp;
+
+
+namespace ArtPicker
+{
+    public partial class artFinder : Form
+    {
+        int secs = 0;
+        int totalTime = 0;
+        int i = 0;
+        string currentPictureURL;
+        FormWindowState LastWindowState = FormWindowState.Minimized;
+        Image retrieved;
+        WebClient wc = new WebClient();
+        
+
+        public artFinder()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addWordToList();
+        }
+
+        private void wordBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)//allows enter to add word to list
+            {
+                addButton_Click(this, new EventArgs());
+                e.Handled = true;
+                e.SuppressKeyPress = true; //gets rid of the ding from hitting enter
+            }
+        }
+
+        private void addWordToList()
+        {
+            if (!string.IsNullOrWhiteSpace(wordBar.Text))
+            {
+                wordList.Items.Add(wordBar.Text);
+                wordBar.Text = "";
+                wordBar.PlaceholderText = "";
+            }
+            else MessageBox.Show("Add words", "Error");
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {   //If wordlist is not empty
+            if (!(wordList.Items.Count == 0) && timerInput.Value + timerInputSeconds.Value > 0)
+            {
+                disableProperties();
+                displayImage(i);
+                timerStart();
+
+            }
+            else MessageBox.Show("Add words/time", "Error");
+
+        }
+
+        private void disableProperties()//disables features 
+        {
+            timerInput.Enabled = false; wordBar.Enabled = false; addButton.Enabled = false; startButton.Enabled = false; deleteButton.Enabled = false; deleteButton.Visible = false;
+            label1.Enabled = false; label2.Enabled = false; wordList.Enabled = false; timerInputSeconds.Enabled = false; skipButton.Enabled = true; skipButton.Visible = true;
+            linkLabel1.Visible = true;
+        }
+
+        private void enableProperties()//enables features 
+        {
+            timerInput.Enabled = true; wordBar.Enabled = true; addButton.Enabled = true; startButton.Enabled = true; deleteButton.Enabled = true; deleteButton.Visible = true;
+            label1.Enabled = true; label2.Enabled = true; wordList.Enabled = true; timerInputSeconds.Enabled = true; skipButton.Enabled = false; skipButton.Visible = false;
+            linkLabel1.Visible = false;
+        }
+        private void timerStart()
+        {
+            secs = 0;
+            totalTime = (int)timerInput.Value * 60 + (int)timerInputSeconds.Value;
+            timerDisplay.Visible = true; wordDisplay.Visible = true;
+            timerDisplay.Text = totalTime.ToString();
+            timer1.Start();
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            secs++;
+            timerDisplay.Text = (totalTime - secs).ToString();
+            if (totalTime - secs <= 0)
+            {
+                timer1.Stop();
+                if (i < wordList.Items.Count - 1)
+                {
+                    i++;
+                    displayImage(i);
+                    timerStart();
+                }
+                else
+                {
+                    enableProperties();
+                    reset();
+                }
+            }
+        }
+        
+        private bool timerRunning()
+        {
+            if (totalTime - secs <= 0)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        private async void testButton_Click(object sender, EventArgs e)
+        {
+            double test = 614.0 / 2821.0;
+            MessageBox.Show(test.ToString());
+
+
+        }
+
+        
+        private void displayImage(int i)
+        {
+            ArrayList wordListArray = wordListToArray(wordList);
+            searchUnsplash((string)wordListArray[i]);
+            wordDisplay.Text = "Displaying word: " + wordListArray[i].ToString();
+            
+        }
+
+        private ArrayList wordListToArray(ListBox wordList)
+        {
+            ArrayList tempArray = new ArrayList();
+            foreach(object item in wordList.Items)
+            {
+                tempArray.Add(item);
+            }
+            return tempArray;
+        }
+
+        //Retrieves random image from Unsplash using given query and resizes it to fit the picture box 
+        private async void searchUnsplash(string query)
+        {
+            try
+            {
+                var client = new UnsplasharpClient("S4_WnRvhDNt93pJAgwvNzRk62xhfAr9F99VYlDTcatM");
+                var photosFound = await client.SearchPhotos(query);
+                int max = photosFound.Count;
+                Random random = new Random();
+                int rnd = random.Next(max);
+                string temp = photosFound[rnd].Urls.Raw; currentPictureURL = photosFound[rnd].Urls.Raw; //+ "&w="+ pictureBox1.Size.Width +"&h=" + pictureBox1.Size.Width;   //+ "&w=540&h=540"
+
+                var request = WebRequest.Create(temp);
+
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                {
+                    retrieved = Bitmap.FromStream(stream);
+                    
+                    if (retrieved.Height >= pictureBox1.Height)
+                    {
+                        double x = ((double)retrieved.Height / (double)pictureBox1.Height);
+                        
+
+                        Image resizedImage =(Image) ResizeImage(retrieved,(int)(retrieved.Width / x), (int)(retrieved.Height / x));
+                        pictureBox1.Image = resizedImage;
+                    }
+                    else if (retrieved.Width >= pictureBox1.Width)
+                    {
+                        double y = ((double)retrieved.Width / (double)pictureBox1.Width);
+                        Image resizedImage = (Image)ResizeImage(retrieved, (int)(retrieved.Width / y), (int)(retrieved.Height / y));
+                        pictureBox1.Image = resizedImage;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = retrieved;
+                    }
+
+                    linkLabel1.LinkVisited = false;
+
+                }
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                MessageBox.Show("No image with query: " + query);
+                timer1.Stop();
+                i++;
+                displayImage(i);
+                timerStart();
+            }
+            
+        }
+
+        private void reset()
+        {
+            secs = 0;
+            totalTime = 0;
+            i = 0;
+            pictureBox1.Image = null;
+            wordList.Items.Clear();
+            timerDisplay.Visible = false;
+            wordDisplay.Visible = false;
+            wordDisplay.Text = "";
+            linkLabel1.Visible = false;  linkLabel1.LinkVisited = false;
+
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            for (int i = wordList.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                wordList.Items.RemoveAt(wordList.SelectedIndices[i]);
+            }
+        }
+
+        private void skipButton_Click(object sender, EventArgs e)
+        {
+            secs = totalTime;
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            Process.Start(new ProcessStartInfo(currentPictureURL) { UseShellExecute = true });
+        }
+
+        private void artFinder_Resize(object sender, EventArgs e)
+        {
+            try
+            {
+                // When window state changes
+                if (WindowState != LastWindowState)
+                {
+                    LastWindowState = WindowState;
+
+
+                    if (WindowState == FormWindowState.Maximized)
+                    {
+                        if (retrieved.Height >= pictureBox1.Height)
+                        {
+                            double x = ((double)retrieved.Height / (double)pictureBox1.Height);
+
+
+                            Image resizedImage = (Image)ResizeImage(retrieved, (int)(retrieved.Width / x), (int)(retrieved.Height / x));
+                            pictureBox1.Image = resizedImage;
+                        }
+                        else if (retrieved.Width >= pictureBox1.Width)
+                        {
+                            double y = ((double)retrieved.Width / (double)pictureBox1.Width);
+                            Image resizedImage = (Image)ResizeImage(retrieved, (int)(retrieved.Width / y), (int)(retrieved.Height / y));
+                            pictureBox1.Image = resizedImage;
+                        }
+                        else
+                        {
+                            pictureBox1.Image = retrieved;
+                        }
+                        // Maximized!
+                    }
+                    if (WindowState == FormWindowState.Normal)
+                    {
+                        if (retrieved.Height >= pictureBox1.Height)
+                        {
+                            double x = ((double)retrieved.Height / (double)pictureBox1.Height);
+
+
+                            Image resizedImage = (Image)ResizeImage(retrieved, (int)(retrieved.Width / x), (int)(retrieved.Height / x));
+                            pictureBox1.Image = resizedImage;
+                        }
+                        else if (retrieved.Width >= pictureBox1.Width)
+                        {
+                            double y = ((double)retrieved.Width / (double)pictureBox1.Width);
+                            Image resizedImage = (Image)ResizeImage(retrieved, (int)(retrieved.Width / y), (int)(retrieved.Height / y));
+                            pictureBox1.Image = resizedImage;
+                        }
+                        else
+                        {
+                            pictureBox1.Image = retrieved;
+                        }
+                        // Restored!
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
